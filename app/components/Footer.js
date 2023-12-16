840467af6f6a40b50294d6598d9a22a564c4732b
Mahdee Hasan <85308927+mahdeehsn@users.noreply.github.com>
2023-12-16 19:19:09 +0000

    sanity set up

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
   return (
     <div className='p-10'>
-        <h1 className='text-center text-lg'>Made By Coding With Douglas</h1>
+      <h1 className='text-center text-lg'>Code is here <Link className='text-blue-400' href={"https://github.com/mahdeehsn/artshop"}>github/mahdeehsn </Link></h1>
     </div>
   )
 }
